790fc73ebe62f45c376c7fac52772e5ae529f7e7
rob587 <robertocamm02@yahoo.com>
2026-03-31 14:33:25 +0200

    feat: implement dark mode functionality using AsyncStorage

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const caricaTema = async () => {
+      const salvato = await AsyncStorage.getItem("darkmode");
+      if (salvato !== null) setDarkMode(JSON.parse(salvato));
+    };
+    caricaTema();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
